48d224446cfc41dc9c810a15c48d649ed698a439
Shiva Mehta <shivamehta73@gmail.com>
2023-11-01 20:53:43 +0530

    added stored procedure methods

--- DAO/InventoryBL.cs
+++ DAO/InventoryBL.cs
@@ -11,23 +11,46 @@ namespace ADOCOREWEBAPP.DAO
 		{
 			Configuration = configuration;
 		}
-		public void AddInventory(Inventory inventory)
+		//public void AddInventory(Inventory inventory)
+		//{
+		//	string connectionString = Configuration["connectionStrings:DefaultConnection"];
+		//	SqlConnection connection = new SqlConnection(connectionString);
+		//	string query = "select * from Inventory";
+		//	SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+		//	SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+		//	DataTable dataTable = new DataTable();
+		//	dataAdapter.Fill(dataTable);
+
+		//	DataRow dataRow = dataTable.NewRow();
+		//	dataRow[1] = inventory.Name;
+		//	dataRow[2] = inventory.Price;
+		//	dataRow[3] = inventory.Quantity;
+		//	dataTable.Rows.Add(dataRow);
+		//	dataAdapter.UpdateCommand = commandBuilder.GetInsertCommand();
+		//	dataAdapter.Update(dataTable);
+
+		//}
+
+		public void AddInventory(Inventory inventory)   //using stored procedure
 		{
 			string connectionString = Configuration["connectionStrings:DefaultConnection"];
-			SqlConnection connection = new SqlConnection(connectionString);
-			string query = "select * from Inventory";
-			SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
-			SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
-			DataTable dataTable = new DataTable();
-			dataAdapter.Fill(dataTable);
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				var query = "InsertInventory";
+				using (SqlCommand command = new SqlCommand(query, connection))
+				{
+					command.CommandType = CommandType.StoredProcedure;
 
-			DataRow dataRow = dataTable.NewRow();
-			dataRow[1] = inventory.Name;
-			dataRow[2] = inventory.Price;
-			dataRow[3] = inventory.Quantity;
-			dataTable.Rows.Add(dataRow);
-			dataAdapter.UpdateCommand = commandBuilder.GetInsertCommand();
-			dataAdapter.Update(dataTable);
+					// Add parameters to the stored procedure
+					command.Parameters.Add(new SqlParameter("@Name", inventory.Name));
+					command.Parameters.Add(new SqlParameter("@Price", inventory.Price));
+					command.Parameters.Add(new SqlParameter("@Quantity", inventory.Quantity));
+					command.Parameters.Add(new SqlParameter("@AddedOn", inventory.AddedOn));
+
+					command.ExecuteNonQuery();
+				}
+			}
 
 		}
 
@@ -56,30 +79,80 @@ namespace ADOCOREWEBAPP.DAO
 			}
 		}
 
-		public IEnumerable<Inventory> GetInventories()
+		//public IEnumerable<Inventory> GetInventories()
+		//{
+		//	string connectionString = Configuration["connectionStrings:DefaultConnection"];
+		//	List<Inventory> inventories = new List<Inventory>();   // since we need to show multiple values
+
+		//	SqlConnection connection = new SqlConnection(connectionString);
+		//	string query = "select* from Inventory";
+		//	SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+		//	DataTable dataTable = new DataTable();
+		//	dataAdapter.Fill(dataTable);
+		//          foreach (DataRow datarow in dataTable.Rows)       //we are converting because these are of object type 
+		//          {
+		//		Inventory inventory = new Inventory(); 
+		//		inventory.Id = Convert.ToInt32(datarow[0]);
+		//		inventory.Name = datarow[1].ToString();
+		//		inventory.Price = decimal.Parse(datarow[2].ToString());
+		//		inventory.Quantity = int.Parse(datarow[3].ToString());
+		//		inventories.Add(inventory);
+		//          }
+
+		//          return inventories;
+
+		//}
+
+		public IEnumerable<Inventory> GetInventories()// using stored procedure
 		{
 			string connectionString = Configuration["connectionStrings:DefaultConnection"];
-			List<Inventory> inventories = new List<Inventory>();   // since we need to show multiple values
+			List<Inventory> inventories = new List<Inventory>();
 
-			SqlConnection connection = new SqlConnection(connectionString);
-			string query = "select* from Inventory";
-			SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
-			DataTable dataTable = new DataTable();
-			dataAdapter.Fill(dataTable);
-            foreach (DataRow datarow in dataTable.Rows)       //we are converting because these are of object type 
-            {
-				Inventory inventory = new Inventory(); 
-				inventory.Id = Convert.ToInt32(datarow[0]);
-				inventory.Name = datarow[1].ToString();
-				inventory.Price = decimal.Parse(datarow[2].ToString());
-				inventory.Quantity = int.Parse(datarow[3].ToString());
-				inventories.Add(inventory);
-            }
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				using (SqlCommand command = new SqlCommand("inventories", connection))
+				{
+					command.CommandType = CommandType.StoredProcedure;
+					DataSet ds = new DataSet();
+					SqlDataAdapter adapter = new SqlDataAdapter(command);
+					adapter.Fill(ds);
+					connection.Close();
 
-            return inventories;
+					if(ds.Tables.Count > 0 && ds.Tables[0].Rows.Count>0)
+					{
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                        {
+							var dr = ds.Tables[0].Rows[i];
+							var obj = new Inventory
+							{
+								Id = Convert.ToInt32(dr["Id"]),
+								Name = dr["Name"].ToString(),
+								Price = Convert.ToDecimal(dr["Price"]),
+								Quantity = Convert.ToInt32(dr["Quantity"]),
+								AddedOn = Convert.ToDateTime(dr["AddedOn"])
+							};
+						inventories.Add(obj);
+						}
+                    }
+					//using (SqlDataReader reader = command.ExecuteReader())
+					//{
+					//	while (reader.Read())
+					//	{
+					//		Inventory inventory = new Inventory();
+					//		inventory.Id = reader.GetInt32(0);
+					//		inventory.Name = reader.GetString(1);
+					//		inventory.Price = reader.GetDecimal(2);
+					//		inventory.Quantity = reader.GetInt32(3);
+					//		inventories.Add(inventory);
+					//	}
+					//}
+				}
+			}
 
+			return inventories;
 		}
-
+		
 		public Inventory GetInventory(int id)
 		{
 			Inventory inventory = new Inventory();
